104bbc4ee6e5dd84f181a5c9704876f3dcb09622
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-09-06 09:44:02 +0800

    add projects

---
+++ Infrastructure/XH.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XH.Infrastructure.Extensions
+{
+    /// <summary>
+    /// String extensions
+    /// </summary>
+    public static class StringExtensions
+    {
+    }
+}
