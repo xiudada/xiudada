b69fbca113593c48e5abb4c34bd7b9db329a1046
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-09-06 17:44:09 +0800

    add more projects

--- Domain/XH.Domain.Models/Articles/Category.cs
+++ Domain/XH.Domain.Models/Articles/Category.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XH.Infrastructure.Domain.Models;
 
 namespace XH.Domain.Models.Article
 {
     /// <summary>
     /// Category
     /// </summary>
-    public class Category
+    public class Category : FullAuditableEntityBase
     {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary>
+        /// Description
+        /// </summary>
+        public virtual string Description { get; set; }
+
+        /// <summary>
+        /// Parent category
+        /// </summary>
+        public virtual Category ParentCategory { get; set; }
     }
 }
