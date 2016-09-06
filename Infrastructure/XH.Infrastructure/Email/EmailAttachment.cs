0464e4236f6fd42e27da69a84dd5e2f4b7d4a111
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-09-06 18:08:03 +0800

    add email base

---
+++ Infrastructure/XH.Infrastructure/Email/EmailAttachment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XH.Infrastructure.Email
+{
+    /// <summary>
+    /// Email attachment
+    /// </summary>
+    public class EmailAttachment
+    {
+        /// <summary>
+        /// Base64
+        /// </summary>
+        public bool Base64 { get; set; }
+
+        /// <summary>
+        /// Content
+        /// </summary>
+        public string ContentUrl { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Type
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
---
+++ Infrastructure/XH.Infrastructure/Email/IEmailProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XH.Infrastructure.Email
+{
+    /// <summary>
+    /// Email provider interface
+    /// </summary>
+    public interface IEmailProvider
+    {
+        /// <summary>
+        /// Send a mail message
+        /// </summary>
+        /// <param name="message"></param>
+        void SendEmail(EmailMessage message);
+    }
+}
