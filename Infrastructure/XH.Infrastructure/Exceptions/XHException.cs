820a91282fd0cc0f7579cbf4cdc89a19183d9443
qinglu@kooboo.cn <qinglu@kooboo.cn>
2016-12-06 17:59:36 +0800

    add repository

---
+++ Infrastructure/XH.Infrastructure/Exceptions/XHException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XH.Infrastructure.Exceptions
+{
+    [Serializable]
+    public class XHException : Exception
+    {
+        /// <summary>
+        /// Creates a new <see cref="XHException"/> object.
+        /// </summary>
+        public XHException()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="XHException"/> object.
+        /// </summary>
+        public XHException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="XHException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public XHException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="XHException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Inner exception</param>
+        public XHException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
